04ca770c0303f47ed4ee0552878656de50e6a5fb
Cristofy Meirú <meiru.cristofy05@gmail.com>
2026-02-14 19:02:24 -0300

    feat: auth-client initialized

---
+++ client/src/lib/auth-client/index.ts
@@ -0,0 +1,9 @@
+import { ENV } from "@/env";
+import { adminClient } from "better-auth/client/plugins";
+import { createAuthClient } from "better-auth/react";
+
+export const authClient = createAuthClient({
+  baseURL: ENV.VITE_API_URL,
+  basePath: "api",
+  plugins: [adminClient()],
+});
